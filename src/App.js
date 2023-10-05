0c647fb78028d365f9b4327d0d67a5656878e341
Tan Vo <tan.vo@kitchenwarehouse.com.au>
2023-10-05 11:14:05 +0700

    change image to monitor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 
 function App() {
   const thumbnail =
-    "image/upload/Supplier%20Images%20/2000px/KitchenAid-Artisan-KSM70-Bowl-Lift-Mixer-Pebbled-Palm_1_2000px.jpg?imagetype=pdp_thumbnail";
+    "image/upload/c_fill,g_face,w_106/f_auto/Supplier%20Images%20/2000px/Villeroy-Boch-Rose-Garden-Sugar-Pot-270ml_1_2000px.jpg?imagetype=pdp_thumbnail";
+
   const gallery =
-    "image/upload/Supplier%20Images%20/2000px/KitchenAid-Artisan-KSM70-Bowl-Lift-Mixer-Pebbled-Palm_1_2000px.jpg?imagetype=pdp_gallery";
+    "image/upload/c_fill,g_face,w_106/f_auto/Supplier%20Images%20/2000px/Villeroy-Boch-Rose-Garden-Sugar-Pot-270ml_1_2000px.jpg?imagetype=pdp_gallery";
   const full =
-    "image/upload/Supplier%20Images%20/2000px/KitchenAid-Artisan-KSM70-Bowl-Lift-Mixer-Pebbled-Palm_1_2000px.jpg?imagetype=pdp_full";
+    "image/upload/c_fill,g_face,w_1250/f_auto/t_PDP_2000x2000/Supplier%20Images%20/2000px/Villeroy-Boch-Rose-Garden-Sugar-Pot-270ml_1_2000px.jpg?imagetype=pdp_full";
+
   return (
     <div className="App bg-gray-200">
       <div className="container mx-auto">
